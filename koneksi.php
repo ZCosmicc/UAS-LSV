1a5e66c5333724f67985db297c1b98f91c2b6b83
Fariz Fadillah <farizfadillah612@gmail.com>
2023-06-15 16:41:13 +0800

    bismillah ya allah

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,8 @@
 <?php
-//Connect database
+//Connect database local
+// $koneksi = mysqli_connect('localhost', 'root', '', 'simp', 3306);
+
+//Connect database server
 $koneksi = mysqli_connect('10.0.0.60', 'fariz', 'Fariz123!', 'simp', 3306);
 
 //Check connection
